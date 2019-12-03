2036cf275090d6eb2833c56cf74e12fb09654138
Arian Rahman <arian.rahman@northsouth.edu>
2019-12-03 13:40:52 +0600

    Add files via upload

---
+++ signup01.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<?php
+session_start();
+$_SESSION['message']='';
+$mysqli = new mysqli('localhost','root','','user');
+
+
+ ?>
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name ="viewport" content=" width= device-width">
+    <title>EZ Life | Sign Up </title>
+    <link rel="stylesheet" href ="./css/style.css">
+
+  </head>
+  <body>
+    <header>
+      <div class ="container">
+        <div id ="branding ">
+          <h1><span class="highlight">EZ</span> Life Project</h1>
+          </div>
+          <nav id ="navigation">
+            <ul>
+              <li> <a href="index.html">Home </a> </li>
+              <li> <a href="login.html">Log in</li>
+              <li class="current"> <a href="signup.html">Sign up</li>
+              <li> <a href= "about.html">About</a> </li>
+            </ul>
+          </nav>
+          </div>
+    </header>
+
+    <section id = "showcase_signup" >
+          <div class = "signup-box">
+
+            <h1>Sign Up as User </h1>
+              <form class="quote" action="signup01.php" method="POST">
+                <div class="textbox">
+                  <br>
+                  <input type="text" placeholder="Name" name="U_name" required>
+                </div>
+                <div class="textbox">
+                  <input type="email" placeholder="Email" name="U_email" required>
+                </div>
+                <div class="textbox">
+                <input type="password" placeholder="Passsword" name="U_password" required>
+                </div>
+                <div class="textbox">
+                <input type="text" placeholder="Phone No." name="U_phone" required>
+                </div>
+                <div class="textbox">
+                <input type="text" placeholder="NID" name="U_nid" required>
+                </div>
+                <div class="textbox">
+                <input type="text" placeholder="House No, Block, Road, Area, Dhaka" name="U_address" required>
+                </div> <br>
+                <button class="button_1" type="submit">Submit</button>
+              </form>
+              </div>
+
+      </section>
+
+
+
+
+    <footer>
+      <p>EZ Life Co. &copy; 2019</p>
+    </footer>
+  </body>
+</html>
